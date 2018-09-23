13fba78de66b2ab2b6cf6f1ed1a9abbbfbb43ca4
ycg <yangchengguo@beidouapp.com>
2018-09-23 23:35:28 +0800

    add project

---
+++ live/compile-Clear.bat
@@ -0,0 +1,16 @@
+@echo **Clear builded files**
+cd liveMedia
+	del *.obj
+cd ../groupsock
+	del *.obj
+cd ../UsageEnvironment
+	del *.obj
+cd ../BasicUsageEnvironment
+	del *.obj
+cd ../testProgs
+	del *.obj
+cd ../mediaServer
+	del *.obj
+cd ..
+@echo **Finished Clear builded files**
+pause
